6111d6b0970241894d82ccfdb9243518fdedbccf
bigbad <x18825407105@outlook.com>
2026-02-23 23:24:55 +0800

    Create 404.vue

---
+++ src/views/error/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="error-page">
+    <h2>404 - 页面未找到</h2>
+    <a-button type="primary" @click="$router.push('/')">返回首页</a-button>
+  </div>
+</template>
+<style scoped>
+.error-page { text-align: center; padding-top: 100px; }
+</style>
