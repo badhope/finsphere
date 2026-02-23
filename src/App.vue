a165e52463d4528a11a7737983cb52313a9ccfb1
bigbad <x18825407105@outlook.com>
2026-02-23 22:46:21 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+/* 全局重置样式 */
+body { margin: 0; padding: 0; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; }
+#app { height: 100vh; }
+</style>
