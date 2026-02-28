ff7ddb790d0130b3cc7c82a4cfac42fafdfdd9a9
FinSphere Developer <dev@finsphere.com>
2026-02-28 20:53:21 +0800

    Initial commit: Clean start for FinSphere Pro

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style>
-/* 可以留空或添加全局重置 */
-</style>
--- src/mock/index.ts
+++
@@ -1,8 +0,0 @@
-import { setupUserMock } from './user';
-import { setupDashboardMock } from './dashboard';
-
-export function setupMock() {
-  setupUserMock();
-  setupDashboardMock();
-  console.log('[Mock] 数据模拟服务已启动');
-}
